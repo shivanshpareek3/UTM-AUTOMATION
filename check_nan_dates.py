1a6320696433fa4aa0d8a6432fa6cdffe4a7b5a5
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-08-31 11:40:17 +0530

    Clean up root directory and sync

--- check_nan_dates.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from src.normalization import parse_date_series
-
-df = pd.read_csv('/Users/apple/Downloads/12-08-2026_leads.csv')
-dates = parse_date_series(df['Order Date'])
-nan_count = dates.isna().sum()
-print("NaN dates:", nan_count)
